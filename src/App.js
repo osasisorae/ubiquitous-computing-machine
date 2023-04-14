560fb123623c1c7cdc22400ec8c66929ae08a5ae
osasisorae <osasisorae@gmail.com>
2023-04-14 13:30:08 +0100

    Changes to be committed: 	modified:   README.md modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.js 	deleted:    src/components/GenerateLessonPlan.js 	modified:   src/components/Home.js 	deleted:    src/components/Login.js 	deleted:    src/components/Register.js 	deleted:    src/components/RegisterForm.js

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const Home = lazy(() => import('./components/Home'));
-const Register = lazy(() => import('./components/Register'));
-const Login = lazy(() => import('./components/Login'));
-const GLP = lazy(() => import('./components/GenerateLessonPlan'));
 
 function App() {
   return (
@@ -13,9 +10,6 @@ function App() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/glp" element={<GLP />} />
         </Routes>
       </Suspense>
   </Router>
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,170 +1,37 @@
-import React, { useState, useEffect } from "react";
-import { makeStyles } from 'tss-react/mui'
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+import '../App.css';
 
-import {
-  Button,
-  Avatar,
-  Card,
-  CardContent,
-  CardHeader,
-  Divider,
-  List,
-  ListItem,
-  ListItemAvatar,
-  ListItemText,
-  Typography,
-} from "@mui/material";
 
-import { Link } from 'react-router-dom';
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: 800,
-    margin: "auto",
-    marginTop: theme.spacing(4),
-  },
-  cardHeader: {
-    textAlign: "center",
-    backgroundColor: theme.palette.primary.main,
-    color: theme.palette.common.white,
-  },
-  projectsHeader: {
-    marginBottom: theme.spacing(1),
-    marginTop: theme.spacing(4),
-  },
-  button: {
-    marginLeft: theme.spacing(1),
-  },
-}));
-
-function Home() {
-  const classes = useStyles();
-  const history = useNavigate();
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    
-    // Check local storage for token
-    const token = localStorage.getItem("accessToken");
-    if (token) {
-      // Use token to get user data
-      fetch("http://127.0.0.1:8000/api/v1/user/", {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          setUser(data);
-        })
-        .catch(error => console.error(error));
-    }
-  }, []);
-
-  const handleLogout = () => {
-    // Remove token from local storage and set user state to null
-    localStorage.removeItem('accessToken');
-    history('/login');
-  };
-
-  const handleLessonPlan = () => {
-    history("/glp");
-  };
-
+function App() {
+  
   return (
-    <div className={classes.root}>
-      {user ? (
-        <Card>
-          <CardHeader
-            className={classes.cardHeader}
-            avatar={<Avatar>{user.username.charAt(0)}</Avatar>}
-            title={user.first_name + " " + user.last_name}
-            subheader={user.email}
-          />
-          <Button variant="contained" color="primary" onClick={handleLogout}>
-              Logout
-            </Button>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              Welcome to alis, the robot teacher.
-            </Typography>
-            <Typography variant="body1" component="p">
-              Supports education by teaching both teachers and students, grading assignments, providing lesson plans and resources, and handling administrative tasks.
-            </Typography>
-            <Divider className={classes.projectsHeader} />
-            <Typography
-              variant="h5"
-              component="h2"
-              className={classes.projectsHeader}
-            >
-              About the Builders
-            </Typography>
-            <List>
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>OI</Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Osarenren Isorae"
-                  secondary="Lead Software Engineer"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>YL</Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Yanru Liu"
-                  secondary="Lead Product Designer"
-                />
-              </ListItem>
-              <ListItem>
-                <ListItemAvatar>
-                  <Avatar>YL</Avatar>
-                </ListItemAvatar>
-                <ListItemText
-                  primary="Yujie Liu"
-                  secondary="Lead UX Designer"
-                />
-              </ListItem>
-            </List>
-          </CardContent>
-
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Generate Lesson Plan - Beta!
-            </Typography>
-            <Button variant="contained" color="primary" onClick={handleLessonPlan}>
-              Start
-            </Button>
-          </CardContent>          
-          </Card>
-
-          
-      ) : (
-        <>
-        <Button
-            component={Link}
-            to="/login"
-            variant="contained"
-            color="primary"
-          >
-            Login
-          </Button>
-          <Button
-            component={Link}
-            to="/register"
-            variant="contained"
-            color="secondary"
-            className={classes.button}
-          >
-            Register
-          </Button>
-        </>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <h1>Revolutionize Education with Alis - The Robot Teacher</h1>
+        <p>Say goodbye to grading papers and hello to personalized lesson plans and resources.</p>
+        <button className="btn-lg">Pre-order Alis Now (Physical + Software)</button>
+        <br />
+        <button className="btn-lg">Pre-order Alis Now (Software)</button>
+        <br />
+        <p>Alis - The Robot Teacher</p>
+        <p>Alis is a cutting-edge educational tool that uses AI to support both teachers and students. Alis can grade assignments, provide personalized lesson plans and resources, and handle administrative tasks, freeing up teachers to focus on what they do best - teaching.</p>
+        <button className="btn-lg">Learn More About Alis</button>
+        <br />
+        <p>Test Our Beta Products</p>
+        <p>Try out our beta products to see how Alis can revolutionize your classroom:</p>
+        <button className="btn-lg">Test Lesson Planner Chatbot (Beta)</button>
+        <br />
+        <button className="btn-lg">Test Resource Generator (Beta)</button>
+        <br />
+        <p>About Us</p>
+        <p>We are pedagogueAI - a team of educators, developers, and designers dedicated to improving education through technology. Follow @_buildspace and @pedagogueAI for the latest news and updates on Alis and our other projects.</p>
+        <button className="btn-lg">Learn More About Our Team</button>
+        <br />
+        <p>Register For Our Monthly Newsletter (Free)</p>
+        <button className="btn-lg">Register Now</button>
+      </header>
     </div>
   );
 }
 
-export default Home;
+export default App;
